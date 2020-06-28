939cb7e59a7cf996fc0352011c13629cc8fd6fc5
David Kirkham <davek17@hotmail.com>
2020-06-28 16:45:54 +0100

    Added source code

---
+++ src/SwaggerDemo/Controllers/GreetingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SwaggerDemo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Greeting")]
+    public class GreetingController : Controller
+    {
+   
+        public IActionResult SayHello([FromQuery] string name)
+        {
+            return Ok($"Hello {name}");
+        }
+
+
+        public IActionResult SayHelloByPost([FromBody] string name)
+        {
+            return Ok($"Hello {name}");
+        }
+    }
+}
